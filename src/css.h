d5bf2fa43adf21a5e79f67a7346567ead3d79cce
meh <meh.ffff@gmail.com>
2009-05-07 00:10:11 +0200

    :OOOOOOOOOOOOOOOOOOOOOOOO

--- src/css.h
+++ src/css.h
@@ -19,12 +19,73 @@
 #ifndef _LIB_CSS_H
 #define _LIB_CSS_H
 
-typedef struct _CSS {
-} CSS;
+/* Exceptions */
+
+typedef struct _CSSException {
+    unsigned    line;
+    unsigned    code;
+    char*       text;
+} CSSException;
+
+/**
+ * Create a new CSSException object.
+ *
+ * @param   line    Line where the exception has been thrown.
+ * @param   code    The exception code.
+ *
+ * @return  The new CSSException object.
+ */
+CSSException* CSS_NewException (unsigned line, unsigned code);
+
+/**
+ * Destroy a CSSException object.
+ *
+ * @param   exception   The object to destroy.
+ */
+void          CSS_DestroyException (CSSException* exception);
+
+/* Parser */
+
+/* Property */
+
+typedef struct _CSSProperty {
+    char* name;
+    char* value;
+} CSSProperty;
+
+/**
+ * Create a new CSSProperty object.
+ *
+ * @param   name    Name of the property.
+ * @param   value   Value of the property.
+ *
+ * @return  A new CSSProperty object.
+ */
+CSSProperty* CSS_NewProperty (const char* name, const char* value);
+
+/**
+ * Destroy a CSSProperty object.
+ *
+ * @param   property    The object to destroy.
+ */
+void         CSS_DestroyProperty (CSSProperty* property);
+
+/* Node */
 
 typedef struct _CSSNode {
-    int     type;
+    char**          selector;
+    unsigned        selectorNumber;
+    CSSProperty**   properties;
 } CSSNode;
+
+/**
+ * 
+ */
+CSSNode* CSS_NewNode (char** selectors, unsigned number, CSSProperty** properties);
+
+typedef struct _CSS {
+    CSSNode**   content;
+} CSSDocument;
 
 /**
  * Parse a CSS text.
@@ -35,7 +96,7 @@ typedef struct _CSSNode {
  *
  * @return  NULL if func isn't NULL.
  */
-CSS* CSS_Parse (const char* text, void (*func)(CSSNode*));
+CSSDocument* CSS_Parse (const char* text, void (*func)(CSSNode*));
 
 /**
  * Parse a CSS file.
@@ -46,6 +107,6 @@ CSS* CSS_Parse (const char* text, void (*func)(CSSNode*));
  *
  * @return  NULL if func isn't NULL.
  */
-CSS* CSS_ParseFile (const char* path, void (*func)(CSSNode*));
+CSSDocument* CSS_ParseFile (const char* path, void (*func)(CSSNode*));
 
 #endif
