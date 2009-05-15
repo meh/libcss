549bac02c922aac7afca4b358244d46f67dc6d33
meh <meh.ffff@gmail.com>
2009-05-15 20:20:29 +0200

    Yay.

--- include/selector/PseudoClass.h
+++ include/selector/PseudoClass.h
@@ -19,16 +19,65 @@
 #ifndef _LIB_CSS_SELECTOR_PSEUDOCLASS_H
 #define _LIB_CSS_SELECTOR_PSEUDOCLASS_H
 
+/**
+ * This structure represents a tree expression (an+b) in the CSSTreePseudoClass.
+ *
+ * When a is negative only the positive offset is used, this is usefull to get
+ * the first b elements.
+ *
+ * When b is negative it gets the element starting from the end of the base group.
+ *
+ * When b is positive it gets the element starting from the beginning of the base group.
+ */
+typedef struct _CSSTreeExpression {
+    int base;   /**< is the a in the expression */
+    int offset; /**< is the b in the expression */
+} CSSTreeExpression;
+
+CSSTreeExpression* CSS_ParseTreeExpression (const char* expression);
+
+#define CSS_PSEUDOCLASS_IS_NORMAL(pseudoClass)  (pseudoClass->type == CSSNormalPseudoClass)
+#define CSS_PSEUDOCLASS_IS_TREE(pseudoClass)    (pseudoClass->type == CSSTreePseudoClass)
+#define CSS_PSEUDOCLASS_IS_NOT(pseudoClass)     (pseudoClass->type == CSSNotPseudoClass)
+
+/**
+ * CSSPseudoClass types.
+ */
 typedef enum {
-    CSSNormalPseudoClass = 0x01,
-    CSSTreePseudoClass   = 0x02,
-    CSSNotPseudoClass    = 0x04
+    CSSNormalPseudoClass = 0x01, /**< normal pseudo classes */
+    CSSTreePseudoClass   = 0x02, /**< tree pseudo classes (eg. :nth-child) */
+    CSSNotPseudoClass    = 0x04  /**< :not() pseudo class */
 } CSSPseudoClassType;
 
+/**
+ * Structure that represents a CSSPseudoClass.
+ */
 typedef struct _CSSPseudoClass {
-    CSSPseudoClassType type;
-    char*              name;
-    void*              value;
+    int   type;  /**< pseudo class type */
+    char* name;  /**< pseudo class name */
+    void* value; /**< pseudo class data */
 } CSSPseudoClass;
+
+/**
+ * Create a CSSPseudoClass object.
+ *
+ * The name string has to be allocated by the programmer.
+ *
+ * @param   type    The pseudo class type.
+ * @param   name    The pseudo class name.
+ * @param   value   The pseudo class data.
+ *
+ * @return  The new CSSPseudoClass object.
+ */
+CSSPseudoClass* CSS_NewPseudoClass (int type, char* name, void* value);
+
+/**
+ * Destroy a CSSPseudoClass object.
+ *
+ * The name value will be free'd by this function.
+ *
+ * @param   pseudoClass   The object to destroy.
+ */
+void CSS_DestroyPseudoClass (CSSPseudoClass* pseudoClass);
 
 #endif
--- src/selector/PseudoClass.c
+++ src/selector/PseudoClass.c
@@ -17,5 +17,110 @@
 ****************************************************************************/
 
 #include "selector/PseudoClass.h"
+#include "selector/SimpleSelector.h"
 #include "common.h"
+#include <ctype.h>
+
+CSSTreeExpression*
+CSS_ParseTreeExpression (const char* expression)
+{
+    CSSTreeExpression* treeExpression = (CSSTreeExpression*) malloc(sizeof(CSSTreeExpression));
+
+    size_t length = strlen(expression);
+    size_t i      = 0;
+    size_t h;
+    
+    // clean spaces
+    while (i < length && expression[i] == ' ') {
+        i++;
+    }
+
+    // if the string ended throw error
+    if (i == length) {
+        goto tree_expression_parse_error;
+    }
+
+    // check that the next char is accepted
+    if (expression[i] != '+' && expression[i] != '-' && expression[i] != 'n' && !isdigit(expression[i])) {
+        goto tree_expression_parse_error;
+    }
+
+    // set the right value
+    if (expression[i] != 'n') {
+        if (i+2 < length && expression[i+1] == 'n') {
+            if (expression[i] == '+') {
+                treeExpression->base = 1;
+            }
+            else {
+                treeExpression->base = -1;
+            }
+        }
+        else {
+            treeExpression->base = atoi(&expression[i]);
+        }
+    }
+    else {
+        treeExpression->base = 1;
+    }
+
+    // clean spaces
+    while (i < length && expression[i] == ' ') {
+        i++;
+    }
+
+    // if the string ended the offset is 0, return the tree expression
+    if (i == length) {
+        treeExpression->offset = 0;
+        goto tree_expression_parse_end;
+    }
+
+    // if there's no + or - throw an error
+    if (!(expression[i] == '+' || expression[i] == '-')) {
+        goto tree_expression_parse_error;
+    }
+
+    // check that the other chars are digits
+    for (h = i+1; h < length && (isdigit(expression[h]) || expression[h] == ' '); h++);
+
+    if (h < length) {
+        goto tree_expression_parse_error;
+    }
+
+    treeExpression->offset = atoi(&expression[i]);
+
+tree_expression_parse_end:
+    return treeExpression;
+
+tree_expression_parse_error:
+    free(treeExpression);
+    return NULL;
+}
+
+CSSPseudoClass*
+CSS_NewPseudoClass (int type, char* name, void* value)
+{
+    CSSPseudoClass* pseudoClass = (CSSPseudoClass*) malloc(sizeof(CSSPseudoClass));
+    pseudoClass->type           = type;
+    pseudoClass->name           = name;
+    pseudoClass->value          = value;
+
+    return pseudoClass;
+}
+
+void
+CSS_DestroyPseudoClass (CSSPseudoClass* pseudoClass)
+{
+    free(pseudoClass->name);
+
+    if (pseudoClass->value) {
+        if (CSS_PSEUDOCLASS_IS_TREE(pseudoClass)) {
+            CSS_DestroyTreeExpression(pseudoClass->value);
+        }
+        else if (CSS_PSEUDOCLASS_IS_NOT(pseudoClass)) {
+            CSS_DestroySimpleSelector(pseudoClass->value);
+        }
+    }
+
+    free(pseudoClass);
+}
 
